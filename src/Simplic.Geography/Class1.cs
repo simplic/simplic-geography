cda878b11e6911a2b93d2c222632b233f9e9b6b8
tanyel.tuncer@spiegelburg.biz <tanyel.tuncer@spiegelburg.biz>
2020-03-06 17:08:33 +0100

    Initial upload

---
+++ src/Simplic.Geography/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.Geography
+{
+    public class Class1
+    {
+    }
+}
