123cbee30e9f50bfa2474ae6be5450d842d7ffb3
tanyel.tuncer@spiegelburg.biz <tanyel.tuncer@spiegelburg.biz>
2020-03-17 17:30:40 +0100

    Add unit testing

--- src/Simplic.Geography/FederalState/FederalState.cs
+++ src/Simplic.Geography/FederalState/FederalState.cs
@@ -1,15 +1,53 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 namespace Simplic.Geography
 {
-    public class FederalState
+    public class FederalState : IEquatable<FederalState>, IEqualityComparer<FederalState>
     {
+        public static bool operator ==(FederalState left, FederalState right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(FederalState left, FederalState right)
+        {
+            return !left.Equals(right);
+        }
+
         public Guid Guid { get; set; } = Guid.NewGuid();
 
         public string Name { get; set; }
 
         //In which Country is the federal state
         public Guid CountryId { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is FederalState _other)
+                return _other.Guid == Guid;
+
+            return false;
+        }
+
+        public bool Equals(FederalState other)
+        {
+            return other?.Guid == Guid;
+        }
+
+        public bool Equals(FederalState x, FederalState y)
+        {
+            return x?.Guid == y?.Guid;
+        }
+
+        public override int GetHashCode()
+        {
+            return Guid.GetHashCode();
+        }
+
+        public int GetHashCode(FederalState obj)
+        {
+            return obj?.Guid.GetHashCode() ?? 0;
+        }
     }
 }
---
+++ test/Simplic.Geography.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Simplic.Geography.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Simplic.Geography.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("63b40cf8-ad28-405f-b800-a62e845bdc15")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
